8c7e5132a13159e02967b50a0e5e45524954db83
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-01-19 11:35:45 +0100

    Código comentado

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -2,127 +2,121 @@ package cuentas;
 
 public class CCuenta {
 
-
-    /**
-     * 
-     */
-    private String nombre;
-    /**
-     * 
-     */
-    private String cuenta;
-    /**
-     * 
-     */
-    private double saldo;
-    /**
-     * 
-     */
-    private double tipoInterés;
-
-    /**
-     * 
-     */
-    public CCuenta()
-    {
-    }
-
-    /**
-     * @param nom
-     * @param cue
-     * @param sal
-     * @param tipo
-     */
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-    }
-
-    /**
-     * @return
-     */
-    public double estado()
-    {
-        return saldo;
-    }
-
-    /**
-     * @param cantidad
-     * @throws Exception
-     */
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
-    }
-
-    /**
-     * @param cantidad
-     * @throws Exception
-     */
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
-    }
+	/**
+	 * @param nombre del cliente
+	 */
+	private String nombre;
+	/**
+	 * @param número de cuenta del cliente
+	 */
+	private String cuenta;
+	/**
+	 *  @param saldo del cliente
+	 */
+	private double saldo;
+	/**
+	 * @param tipo de interés asociado al cliente
+	 */
+	private double tipoInterés;
 
 	/**
-	 * @return
+	 * Constructor que inicializa los parametros de clase CCuenta
+	 */
+	public CCuenta() {
+	}
+
+	/**
+	 * @param nom nombre del cliente
+	 * @param cue número de cuenta del cliente
+	 * @param sal saldo del cliente
+	 * @param tipo tipo de interés asociado al cliente
+	 */
+	public CCuenta(String nom, String cue, double sal, double tipo) {
+		nombre = nom;
+		cuenta = cue;
+		saldo = sal;
+	}
+
+	/**
+	 * @return Devuelve el saldo actual de la cuenta del cliente
+	 */
+	public double estado() {
+		return saldo;
+	}
+
+	/**
+	 * @param cantidad Comprueba que la cantidad a ingresar es positiva, ya que no tendría sentido un ingreso negativo
+	 * @throws Exception Aviso para que el cliente vea que ha intentado ingresar algo negativo
+	 */
+	public void ingresar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		saldo = saldo + cantidad;
+	}
+
+	/**
+	 * @param cantidad Comprueba que la cantidad a retirar es positiva, y que su valo no supera el límite del saldo que tiene el cliente, ya que no puede "deber" dinero
+	 * @throws Exception Aviso para que el cliente vea que ha intentado retirar una cantidad negativa o más dinero del que dispone
+	 */
+	public void retirar(double cantidad) throws Exception {
+		if (cantidad <= 0)
+			throw new Exception("No se puede retirar una cantidad negativa");
+		if (estado() < cantidad)
+			throw new Exception("No se hay suficiente saldo");
+		saldo = saldo - cantidad;
+	}
+
+	/**
+	 * @return Da acceso a la variable privada nombre
 	 */
 	public String getNombre() {
 		return nombre;
 	}
 
 	/**
-	 * @param nombre
+	 * @param nombre Da valor a la variable privada nombre
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
 	/**
-	 * @return
+	 * @return Da acceso a la variable privada cuenta
 	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
 	/**
-	 * @param cuenta
+	 * @param cuenta Da valor a la variable privada cuenta
 	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
 	/**
-	 * @return
+	 * @return Da acceso a la variable privada saldo
 	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
 	/**
-	 * @param saldo
+	 * @param saldo Da valor a la variable privada saldo
 	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
 	/**
-	 * @return
+	 * @return Da acceso a la variable privada tipoInterés
 	 */
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
 
 	/**
-	 * @param tipoInterés
+	 * @param tipoInterés Da valor a la variable privada tipoInterés
 	 */
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
